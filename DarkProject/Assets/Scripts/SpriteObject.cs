6a4f5d02c4f2da69d1a7124de6d3f2f2aa00440b
FabioArias0 <114270134+FabioArias0@users.noreply.github.com>
2022-12-12 01:04:12 -0400

    Subo proyecto unity
    Se ha descargado algunos prefabs para facilitar la construccion del mapa.
    Además he realizado la construcción de los niveles.

---
+++ DarkProject/Assets/Scripts/SpriteObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+   
+}
