09ca5d674981b4a047f71253815e9dcc68b196bb
Yves Gervens Constant <gervoos@gmail.com>
2022-12-15 02:47:28 -0400

    agrego la animacion para movimiento

--- DarkProject/Assets/Scripts/MoverJugador.cs
+++ DarkProject/Assets/Scripts/MoverJugador.cs
@@ -7,13 +7,14 @@ public class MoverJugador : MonoBehaviour
     public Rigidbody theRB;
     public float velocidadMovimiento;
     public float jumpForce;
+    private Animator animator;
 
     private Vector2 moveInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +26,14 @@ public class MoverJugador : MonoBehaviour
         moveInput.Normalize();
 
         theRB.velocity = new Vector3(moveInput.x * velocidadMovimiento, theRB.velocity.y, moveInput.y*velocidadMovimiento);
+
+        if(moveInput.x == 0 && moveInput.y == 0){
+            return;
+        }else {
+            animator.SetFloat("XInput",moveInput.x);
+        animator.SetFloat("YInput",moveInput.y);
+        }
+
+        
     }
 }
